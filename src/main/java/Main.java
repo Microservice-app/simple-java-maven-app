0dd7e5f4269c62d97138e35851159f7dbd87b10a
Jargielo <weronika.jargielo@accenture.com>
2021-11-08 10:10:12 +0100

    Initial commit.

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(checkIfInputIsAnEvenNumber(22)); // Testing in the main method
+    }
+
+    public static boolean checkIfInputIsAnEvenNumber(int number) {
+        return number % 2 == 0;
+    }
+
+}
+
+
